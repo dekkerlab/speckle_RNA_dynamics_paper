7d335971bab5de57f90657c9b941b325a628c2b1
dlafont15 <50594175+dlafont15@users.noreply.github.com>
2026-08-16 20:01:34 -0400

    Create Fig6_EDFig7.R
    Listing Jupyter notebooks used to generate Fig.6 and Extended Data Fig. 7

---
+++ Fig_6/Fig6_EDFig7.R
@@ -0,0 +1,10 @@
+####### Fig.6a - cCRE pileup/stacks were generated using "cCRE_stacks.ipynb"
+############## - associated region files were generated using "get_cCREregions.ipynb"
+############## - LOS bw were generated using bedGraphToBigWig "e.g. bedGraphToBigWig LOS_residuals_range2Mb_K562_PIIBNT_Mock_R2_20201102_mod.bedGraph ~/data/hg38_noAlt.size LOS_residuals_range2Mb_K562_PIIBNT_Mock_R2_20201102_mod.bw"
+ 
+####### Fig.6bcdfgh - gene-bodies pileup/stacks were generated using "ScaledGeneBody_stacks.ipynb"
+############## - associated region files were generated using "get_geneBodies.ipynb"
+############## - LOS bw were generated using bedGraphToBigWig "e.g. bedGraphToBigWig LOS_residuals_range2Mb_K562_PIIBNT_Mock_R2_20201102_mod.bedGraph ~/data/hg38_noAlt.size LOS_residuals_range2Mb_K562_PIIBNT_Mock_R2_20201102_mod.bw"
+
+
+####### Fig.6e 2D pileups were generated using 'PLS_pileup.ipynb'
